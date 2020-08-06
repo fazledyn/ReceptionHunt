0026e9c7406987862394ac1f96e0bb19886b0e42
Ataf Fazledin <1705066@ugrad.cse.buet.ac.bd>
2020-08-07 01:42:58 +0600

    Models, Urlpattern added

--- Django (New)/hunt/puzzle/models.py
+++ Django (New)/hunt/puzzle/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField(blank=False)
+    level_completed = models.IntegerField()
+    points = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Quiz(models.Model):
+    id = models.IntegerField(primary_key=True)
+    image = models.ImageField(upload_to='puzzle')
+    answer = models.TextField(blank=False)
+
+    def __str__(self):
+        return str( self.id )
+
+
+class Submission(models.Model):
+    submitter = models.OneToOneField(Team, on_delete=models.CASCADE)
+    quiz      = models.OneToOneField(Quiz, on_delete=models.CASCADE)
+    answer    = models.TextField()
+
+    def __str__(self):
+        return self.submitter.name + " to " + str(self.quiz)
